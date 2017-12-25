cdbfed17478eeebd3c3de0fcd2a05f2c248e3853
brian harrison <brian@idiomatic.org>
2017-12-24 21:36:36 -0800

    routes

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const Koa = require('koa')
 const app = module.exports = new Koa()
+const route = require('koa-route')
 const serve = require('koa-static')
 
 app.use(serve("public"))
 
-app.use(async (ctx) => {
-  ctx.body = 'Hello World'
-})
+app.use(route.get('/hello.json', async (ctx) => {
+    ctx.body = { message: 'aloha' }
+}))
 
 if (!module.parent) app.listen(8000)
--- test.js
+++ test.js
@@ -1,11 +1,21 @@
 const test = require('koa-test')
 const app = require('./app')
 const scenario = test(() => app)
+
 scenario({
     title: 'GET /',
     path: '/',
     method: 'get',
     assertions: [
         (res, t) => t.equal(res.status, 200),
-        (res, t) => t.equal(res.text, 'aloha\n')
+        (res, t) => t.equal(res.text, '<p>hello, world.</p>\n')
+    ]})
+
+scenario({
+    title: 'GET /hello.json',
+    path: '/hello.json',
+    method: 'get',
+    assertions: [
+        (res, t) => t.equal(res.status, 200),
+        (res, t) => t.equal(res.text, JSON.stringify({ message:'aloha' }))
     ]})
