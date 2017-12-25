63d73249c7dd4213303ddc7287524ed8c6ec7b1f
brian harrison <brian@rbrian.com>
2017-12-25 03:30:16 +0000

    collect response

--- test.js
+++ test.js
@@ -1,1 +1,9 @@
-1;
+const http = require('http')
+
+http.get('http://localhost:8000', (resp) => {
+    let data = ''
+    resp.on('data', (chunk) => data += chunk)
+    resp.on('end', () => console.log(JSON.parse(data).explanation))
+}).on("error", (err) => {
+    console.log("Error: " + err.message)
+})
