1b2d3e551193445a5ef6cd269b699a63cd2a1e22
brian harrison <brian@rbrian.com>
2017-12-25 00:00:44 +0000

    Update main.js

--- main.js
+++ main.js
@@ -6,8 +6,7 @@ const app = new http.Server()
 
 app.on('request', (req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' })
-  res.write('Hello World')
-  res.end('\n')
+  res.end('Hello, World.\n')
 })
 
 app.listen(port, () => {
