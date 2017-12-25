2ab2f6830bfbafc40eeb9f84b731c7bc3f995643
brian harrison <brian@rbrian.com>
2017-12-25 00:58:25 +0000

    static

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const Koa = require('koa')
 const app = module.exports = new Koa()
+const serve = require('koa-static')
+
+app.use(serve("public"))
 
 app.use(async (ctx) => {
   ctx.body = 'Hello World'
