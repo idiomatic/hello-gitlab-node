6822af3bdc48bda248b17d7111fdb62c2ff68a65
brian harrison <brian@idiomatic.org>
2017-12-29 19:09:07 -0800

    no setup; cassandra ping

--- app.js
+++ app.js
@@ -2,10 +2,17 @@ const Koa = require('koa')
 const app = module.exports = new Koa()
 const route = require('koa-route')
 const serve = require('koa-static')
+const cassandra = require('cassandra-driver')
+const db = new cassandra.Client({
+    contactPoints: ['cassandra'],
+    keyspace: 'system'
+})
 
 app.use(serve("public"))
 
 app.use(route.get('/hello.json', async (ctx) => {
+    let err, result = await db.execute('ping')
+    console.log(err)
     ctx.body = { message: 'aloha' }
 }))
 
